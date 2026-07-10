6a1add85068d3777ae0f7f449fba48ff971bb40d
jadejayuvrajsinh257 <jadejayuvrajsinh257@gmail.com>
2026-07-10 23:50:54 +0530

    pig_game

---
+++ project3_pig_game/script.js
@@ -0,0 +1,9 @@
+'use scripct';
+
+const score0EL = document.querySelector('#score--0');
+const score1EL = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+score0EL.textContent=0;
+score1EL.textContent=0;
+diceEl.classList.add('hidden');
